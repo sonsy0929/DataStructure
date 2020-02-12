4929239fdfa142c7893d35aa51420f5ea35c02ba
heize <sonsy0929@naver.com>
2020-02-12 23:30:47 +0900

    fix: 생성자 내 변수 수정

--- segement tree.cpp
+++ segement tree.cpp
@@ -1,27 +1,28 @@
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 const int SIZE = 1 << 21;
-struct segTree{
+struct segTree {
     long long arr[SIZE];
     int start;
-    segTree(int N){
+    segTree(int N) {
         start = 1;
-        for (; start < SIZE; start *= 2);
+        for (; start < N; start *= 2)
+            ;
         memset(arr, 0, sizeof(arr));
     }
-    void update(int i, int val){
+    void update(int i, int val) {
         i += start;
         arr[i] = val;
-        while (i > 1){
+        while (i > 1) {
             i /= 2;
             arr[i] = arr[i * 2] + arr[i * 2 + 1];
         }
     }
-    // 구간 [L, R)에 대해 
+    // 구간 [L, R)에 대해
     long long getSum(int L, int R) { return getSum(L, R, 1, 0, start); }
-    long long getSum(int L, int R, int node, int nL, int nR){
+    long long getSum(int L, int R, int node, int nL, int nR) {
         if (R <= nL || nR <= L) return 0;
         if (L <= nL && nR <= R) return arr[node];
         int mid = (nL + nR) / 2;
